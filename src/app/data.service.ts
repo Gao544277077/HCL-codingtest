01f384f405cae5a56d5cb9a5252e49ceb3441bf9
Shengwei Gao <shengweigao@Shengweis-MacBook-Pro.local>
2020-11-13 19:41:57 -0500

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable()
+export class DataService {
+
+  private countryNameSource = new BehaviorSubject<string>("");
+  currentCountryName = this.countryNameSource.asObservable();
+
+  constructor() { }
+
+  changeCountryName(country: string) {
+    // add data to an observable
+    this.countryNameSource.next(country);
+    this.countryNameSource.subscribe(name=>console.log(name));
+  }
+ 
+}
